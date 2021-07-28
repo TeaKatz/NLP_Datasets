2f898e99a39f628d5b68f0dab2619e01a6ab56c6
Panuthep <falcon_270394@hotmail.co.th>
2021-07-28 18:55:09 +0700

    Update performance WordDistributionDataset

--- WordDistributionDataset.py
+++ WordDistributionDataset.py
@@ -1,57 +1,79 @@
 import os
 import json
+import joblib
 
 import numpy as np
-
-from tqdm import tqdm
 
 from .BaseDataset import BaseDataset
 from .path_config import meta_word_distribution_corpus_train_dir, word_distribution_corpus_dir
 
 
 def process_meta_word_distribution(context_size: int=4):
-    # Process meta data
-    corpus_dir = word_distribution_corpus_dir + f"/word_distribution_context_size_{context_size}.json"
-    if os.path.exists(corpus_dir):
-        # Load processed meta data
-        with open(corpus_dir, "r") as f:
-            word_distribution = json.load(f)
+    # Get words indexing
+    word_indexing_dir = word_distribution_corpus_dir + f"/word_indexing.pkl"
+    if os.path.exists(word_indexing_dir):
+        # Laod words indexing
+        word_indexing = joblib.load(word_indexing_dir)
     else:
-        print("Processing meta data...")
-        word_distribution = {}
+        print("Creating word indexing...")
+        # Read Meta data
+        with open(meta_word_distribution_corpus_train_dir, "r") as f:
+            meta_word_distribution = json.load(f)
+        # Create word indexing
+        word_indexing = {word: idx for idx, word in enumerate(meta_word_distribution)}
+        # Save words indexing
+        joblib.dump(word_indexing, word_indexing_dir)
 
-        # Read JSON
+    # Get word distribution
+    word_distribution_dir = word_distribution_corpus_dir + f"/word_distribution_context_size_{context_size}.pkl"
+    if os.path.exists(word_distribution_dir):
+        # Load word distribution
+        word_distribution = joblib.load(word_distribution_dir)
+    else:
+        print("Creating word distribution...")
+        # Initial
+        word_distribution = np.zeros([len(word_indexing), len(word_indexing)], dtype=float)
+        word_freq = np.zeros([len(word_indexing)], dtype=int)
+
+        # Read Meta data
         with open(meta_word_distribution_corpus_train_dir, "r") as f:
             meta_word_distribution = json.load(f)
 
         # Get occurrence probability for each word
-        freq_all = sum([meta_word_distribution[word]["freq"] for word in meta_word_distribution])
-        word_probs = {word: meta_word_distribution[word]["freq"] / freq_all for word in meta_word_distribution}
+        for word in meta_word_distribution:
+            word_freq[word_indexing[word]] = meta_word_distribution[word]["freq"]
 
         # Get target word
-        for target_word in meta_word_distribution:
+        for target_word in word_indexing:
+            target_word_index = word_indexing[target_word]
             # Get context words
-            contexts = {}
             for i in range(1, context_size + 1):
                 for context_word, freq in meta_word_distribution[target_word][f"context_{i}"].items():
-                    if context_word in meta_word_distribution:
-                        contexts[context_word] = contexts.get(context_word, 0) + freq
-            contexts = {word: (freq / meta_word_distribution[target_word]["freq"]) / word_probs[word] for word, freq in contexts.items()}
-            # Normalize context's probability
-            prob_all = sum([prob for prob in contexts.values()])
-            contexts = {word: prob / prob_all for word, prob in contexts.items()}
-            # Add to word_distribution
-            word_distribution[target_word] = {"prob": word_probs[target_word], "contexts": contexts}
+                    if context_word in word_indexing:
+                        context_word_index = word_indexing[context_word]
+                        word_distribution[target_word_index, context_word_index] += freq
+            if np.sum(word_distribution[target_word_index]) == 0:
+                word_distribution[target_word_index] = 1 / len(word_distribution[target_word_index])
+            else:
+                # Rescale word distribution
+                word_distribution[target_word_index] = (word_distribution[target_word_index] / word_freq)
+                # Normalize probability
+                word_distribution[target_word_index] = word_distribution[target_word_index] / np.sum(word_distribution[target_word_index])
+        # Save word distribution
+        joblib.dump(word_distribution, word_distribution_dir)
+    return word_indexing, word_distribution
 
-        # Save processed meta data
-        with open(corpus_dir, "w") as f:
-            json.dump(word_distribution, f, indent=4)
-    return word_distribution
 
-
-def load_local_word_distribution(max_samples: int=1000000, context_size: int=4, context_words_num: int=1, non_context_words_num: int=None):
+def load_local_word_distribution(max_samples: int=10000, context_size: int=4, context_words_num: int=1, non_context_words_num: int=5):
     # Get word distribution
-    word_distribution = process_meta_word_distribution(context_size)
+    word_indexing, word_distribution = process_meta_word_distribution(context_size)
+    reversed_word_indexing = {idx: word for word, idx in word_indexing.items()}
+    negative_word_distribution = (word_distribution == 0).astype(float)
+    for target_word_index in range(negative_word_distribution.shape[0]):
+        if np.sum(negative_word_distribution[target_word_index]) == 0:
+            negative_word_distribution[target_word_index] = 1 / len(negative_word_distribution[target_word_index])
+        else:
+            negative_word_distribution[target_word_index] = negative_word_distribution[target_word_index] / np.sum(negative_word_distribution[target_word_index])
 
     # Generate sample
     count = 0
@@ -59,42 +81,36 @@ def load_local_word_distribution(max_samples: int=1000000, context_size: int=4, 
         if count >= max_samples:
             break
         # Get target word
-        for target_word in word_distribution:
+        for target_word, target_idx in word_indexing.items():
             if count >= max_samples:
                 break
             # Get context words
-            context_words = [word for word, _ in word_distribution[target_word]["contexts"].items()]
-            context_probs = [prob for _, prob in word_distribution[target_word]["contexts"].items()]
-            sampling_context_words = np.random.choice(context_words, size=context_words_num, replace=False, p=context_probs).tolist()
+            sampling_context_indices = np.random.choice(range(len(word_indexing)), size=context_words_num, replace=False, p=word_distribution[target_idx])
+            sampling_context_words = [reversed_word_indexing[idx] for idx in sampling_context_indices]
             # Get non-context words
             if non_context_words_num is None:
                 sampling_non_context_words = []
             else:
-                non_context_words = [word for word in word_distribution if word != target_word and word not in context_words]
-                sampling_non_context_words = np.random.choice(non_context_words, size=non_context_words_num, replace=False).tolist()
+                sampling_non_context_indices = np.random.choice(range(len(word_indexing)), size=non_context_words_num, replace=False, p=negative_word_distribution[target_idx])
+                sampling_non_context_words = [reversed_word_indexing[idx] for idx in sampling_non_context_indices]
             count += 1
             yield target_word, sampling_context_words, sampling_non_context_words
 
 
-def load_global_word_distribution(max_samples: int=None, context_size: int=4, context_words_num: int=1000, non_context_words_num: int=None):
+def load_global_word_distribution(max_samples: int=None, context_size: int=4, context_words_num: int=1000):
     # Get word distribution
-    word_distribution = process_meta_word_distribution(context_size)
+    word_indexing, word_distribution = process_meta_word_distribution(context_size)
+    reversed_word_indexing = {idx: word for word, idx in word_indexing.items()}
 
     # Generate sample
-    for i, target_word in enumerate(word_distribution):
+    for i, (target_word, target_idx) in enumerate(word_indexing.items()):
         if max_samples is not None:
             if i >= max_samples:
                 break
         # Get contexts
-        context_words = [word for word, _ in word_distribution[target_word]["contexts"].items()]
-        context_probs = [prob for _, prob in word_distribution[target_word]["contexts"].items()]
-        contexts = sorted(zip(context_words, context_probs), key=lambda x: x[1], reverse=True)[:context_words_num]
-        # Get non-contexts
-        if non_context_words_num is None:
-            non_contexts = []
-        else:
-            non_contexts = [(word, 1 / non_context_words_num) for word in word_distribution if word != target_word and word not in word_distribution[target_word]["contexts"]][:non_context_words_num]
-        yield target_word, contexts, non_contexts
+        sorted_indices = np.argsort(word_distribution[target_idx])[:context_words_num]
+        context_words = [(reversed_word_indexing[idx], word_distribution[target_idx, idx]) for idx in sorted_indices]
+        yield target_word, context_words
 
 
 def load_word(max_samples: int=None):
@@ -116,8 +132,8 @@ class LocalWordDistributionDataset(BaseDataset):
     def __init__(self,
                  context_size: int=4,
                  context_words_num: int=1,
-                 non_context_words_num: int=None,
-                 max_samples: int=1000000,
+                 non_context_words_num: int=5,
+                 max_samples: int=10000,
                  train_split_ratio=0.9,
                  val_split_ratio=0.1,
                  test_split_ratio=0.0,
@@ -148,23 +164,53 @@ class LocalWordDistributionDataset(BaseDataset):
     def _process_data(self, data):
         """ Preprocess and transform data into sample """
         # Extract data
-        target, contexts, non_contexts = data
+        target_word, context_words, non_context_words = data
 
         # Transform data into sample
-        sample = {"input": {"target_word": target, "context_words": contexts, "non_context_words": non_contexts}}
+        sample = {"input": {"target_word": target_word, "context_words": context_words, "non_context_words": non_context_words}}
         return sample
 
 
-class GlobalWordDistributionDataset(LocalWordDistributionDataset):
+class GlobalWordDistributionDataset(BaseDataset):
     local_dir = "global_word_distribution_dataset"
+
+    def __init__(self,
+                 context_size: int=4,
+                 context_words_num: int=1000,
+                 max_samples: int=None,
+                 train_split_ratio=0.9,
+                 val_split_ratio=0.1,
+                 test_split_ratio=0.0,
+                 random_seed: int=0,
+                 local_dir: str=None):
+
+        self.context_size = context_size
+        self.context_words_num = context_words_num
+        super().__init__(max_samples, train_split_ratio, val_split_ratio, test_split_ratio, random_seed, local_dir)
 
     def _load_train(self):
         """ Yield data from training set """
-        for target_word, contexts, non_contexts in load_global_word_distribution(max_samples=self.max_samples, 
-                                                                                 context_size=self.context_size,
-                                                                                 context_words_num=self.context_words_num, 
-                                                                                 non_context_words_num=self.non_context_words_num):
-            yield target_word, contexts, non_contexts
+        for target_word, context_words in load_global_word_distribution(max_samples=self.max_samples, 
+                                                                        context_size=self.context_size,
+                                                                        context_words_num=self.context_words_num):
+            yield target_word, context_words
+
+    def _load_val(self):
+        """ Yield data from validation set """
+        pass
+
+    def _load_test(self):
+        """ Yield data from test set """
+        pass
+
+    def _process_data(self, data):
+        """ Preprocess and transform data into sample """
+        # Extract data
+        target_word, context_words = data
+
+        # Transform data into sample
+        sample = {"input": {"target_word": target_word, "context_words": context_words}}
+        return sample
 
 
 class WordDataset(BaseDataset):

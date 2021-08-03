9f9449bf12df9689ba018b6d356bb0e547a0975b
Panuthep <falcon_270394@hotmail.co.th>
2021-08-04 03:35:40 +0700

    Fixed import bug

--- src/nlp_datasets/word_embedding/SpellingSimilarityDataset.py
+++ src/nlp_datasets/word_embedding/SpellingSimilarityDataset.py
@@ -1,4 +1,4 @@
-from NLP_Metrics import Metrics
+from nlp_metrics import Metrics
 from ..BaseDataset import BaseDataset
 from ..path_config import spelling_similarity_words_dir, spelling_similarity_anagram_dir, spelling_similarity_misspellings_dir
 

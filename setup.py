4b119e0e3438e2a6c5781a29c4fbd8677b7cd2df
Panuthep <falcon_270394@hotmail.co.th>
2022-01-05 20:43:49 +0700

    Update setup

--- setup.py
+++ setup.py
@@ -1,12 +1,7 @@
-import os
-
 from glob import glob
 from os.path import basename, splitext
 from setuptools import find_packages, setup
 
-
-base_path = os.path.dirname(__file__) + "/.."
-nlp_metrics_path = base_path + "/NLP_Metrics/"
 
 setup(
     name="nlp_datasets",
@@ -14,7 +9,4 @@ setup(
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     py_modules=[splitext(basename(path))[0] for path in glob("src/nlp_datasets/*.py")],
-    install_requires=[
-        "nlp_metrics @ file://localhost/%s" % nlp_metrics_path,
-        ],
 )
